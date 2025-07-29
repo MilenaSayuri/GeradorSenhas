968c65e90d0ca415cfaacf11f9989a85d3b155e5
Milena Sayuri Hayakawa <mirenahayakawa@gmail.com>
2025-07-28 22:44:22 -0300

    improvement: adiciona entrada de dados do usuario e salva senha em arquivo

--- script.py
+++ script.py
@@ -1,6 +1,11 @@
 import random
 
-def gerar_senha(tamanho=20):
+tamanho_str = input("Digite o tamanho da senha desejada: ")
+senha = int(tamanho_str)
+site = input("Digite o nome do site: ")
+login = input("Digite o nome de usuario/gmail: ")
+
+def gerar_senha(tamanho):
     string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     number = '123456789'
     simbols = '@%&$'
@@ -17,5 +22,13 @@ def gerar_senha(tamanho=20):
         if maiscula and minuscula and numero and simbolo:
             return ''.join(senha)
 
-senha_completa = gerar_senha()     
-print('Senha: ', senha_completa)
+senha_completa = gerar_senha(senha)
+print("Site:", site)
+print("Login:", login)
+print("Senha gerada:", senha_completa)
+
+with open("senha_gerada.txt", "a") as arquivo:
+    arquivo.write(f"Site: {site}\n")
+    arquivo.write(f"Login: {login}\n")
+    arquivo.write(f"Senha: {senha_completa}\n")
+    arquivo.write("-----------------------\n")
